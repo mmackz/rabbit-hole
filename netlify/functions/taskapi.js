fe9debac0e9d05db8aebfe6adc2eb61f4d5cbafd
Mmackz <matthewmackenz@gmail.com>
2022-02-06 12:20:03 -0800

    📝 remove commented out code

--- netlify/functions/taskapi.js
+++ netlify/functions/taskapi.js
@@ -8,9 +8,6 @@ exports.handler = async function (event) {
       );
       return {
          statusCode: 200,
-         // headers: {
-         //    'Access-Control-Allow-Origin': '*'
-         // },
          body: JSON.stringify(response.data)
       };
    } catch (err) {
