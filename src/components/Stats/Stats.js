8173ad3a9e99dfad7aab9c3368e359178b010507
Mmackz <matthewmackenz@gmail.com>
2022-09-10 11:27:30 -0700

    🎨 style and layout for credentials

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -17,44 +17,49 @@ function Stats({ props }) {
 
    return (
       <section className="stats-outer section">
-         <h1 className="stats-title">User Stats</h1>
          <div className="stats-inner">
             <div className="stat-container">
-               <div className="blockie">
-                  <img src={makeBlockie(hex)} alt="blockie" />
-               </div>
-               <div className="stat-text">
-                  <p className={ensStyle}>
-                     <span className="small-text">ENS:</span>
-                     {ensDisplay}
-                  </p>
-                  <p className="short-address">
-                     <span className="small-text">Address:</span>
-                     <a
-                        className="address-link"
-                        href={`https://etherscan.io/address/${hex}`}
-                     >
-                        {shortAddress}
-                     </a>
-                  </p>
+               <h1 className="stats-title info">Info</h1>
+               <div className="stat-address">
+                  <div className="blockie">
+                     <img src={makeBlockie(hex)} alt="blockie" />
+                  </div>
+                  <div className="stat-text">
+                     <p className={ensStyle}>
+                        <span className="small-text">ENS:</span>
+                        {ensDisplay}
+                     </p>
+                     <p className="short-address">
+                        <span className="small-text">Address:</span>
+                        <a
+                           className="address-link"
+                           href={`https://etherscan.io/address/${hex}`}
+                        >
+                           {shortAddress}
+                        </a>
+                     </p>
+                  </div>
                </div>
             </div>
-
             <div className="stat-container credentials">
-               <div className="credential">
-                  <p className="credential-text">NFT</p>
-                  <img src={nfts} alt="NFT credential" />
-               </div>
-               <div className="credential">
-                  <p className="credential-text">DAO</p>
-                  <img src={daos} alt="DAO credential" />
-               </div>
-               <div className="credential">
-                  <p className="credential-text">DEFI</p>
-                  <img src={defi} alt="DEFI credential" />
-               </div>
-               <div className="credential">
-                  <img src={brightId} alt="DEFI credential" />
+               <h1 className="stats-title">Credentials</h1>
+               <div className="credential-container">
+                  <div className="credential">
+                     <p className="credential-text">NFT</p>
+                     <img src={nfts} alt="NFT credential" />
+                  </div>
+                  <div className="credential">
+                     <p className="credential-text">DAO</p>
+                     <img src={daos} alt="DAO credential" />
+                  </div>
+                  <div className="credential">
+                     <p className="credential-text">DEFI</p>
+                     <img src={defi} alt="DEFI credential" />
+                  </div>
+                  <div className="credential">
+                     <p className="credential-text small-text">BRIGHT ID</p>
+                     <img src={brightId} alt="Bright ID" />
+                  </div>
                </div>
             </div>
          </div>
