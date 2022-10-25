3b4c1d31eb48225b420daa9279fa4b796a0dadb9
Mmackz <matthewmackenz@gmail.com>
2022-10-25 14:10:22 -0700

    ♻️ refactor Tasks component

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,89 +1,22 @@
-import daos from "../../images/credentials/rhdaos.svg";
-import daoImg from "../../images/credentials/daocred.png";
-import nfts from "../../images/credentials/rhnfts.svg";
+import getTaskData from "../../helpers/getTaskData";
+import daoImg from "../../images/credentials/daocredsm.png";
 import nftImg from "../../images/credentials/nftcred.png";
-import defi from "../../images/credentials/rhdefi.svg";
 import defiImg from "../../images/credentials/deficred.png";
-import l2 from "../../images/credentials/rhl2.svg";
 import l2Img from "../../images/credentials/l2cred.png";
-import greenCheck from "../../images/green-check.svg";
+
+import Card from "../Card/Card";
 
 function Tasks(props) {
-   const task = props.data.taskProgress;
-
-   function formatName(name) {
-      return name.replace(/_/g, " ").replace(/(?!\b)[A-Z]/g, (char) => char.toLowerCase());
-   }
-
-   const data = Object.entries(task);
+   const data = Object.entries(props.data.taskProgress);
 
    return (
-      <div className="tasks-section">
-         <section className="credential-card">
-            <div className="card-credential">
-               <img src={daoImg} alt="DAOs" />
-               <img className="checkmark" src={greenCheck} alt="checkmark" />
-            </div>
-
-            <div>
-               <div className="card-title">
-                  <h1 className="form-title">Intro To DAOS</h1>
-               </div>
-
-               <div className="card-tasks">
-                  <div className="card-task">
-                     <p className="card-task-name">Gnosis Safe</p>
-                     <span>{data[1][1].progress ? "✅" : "❌"}</span>
-                  </div>
-               </div>
-            </div>
-         </section>
-      </div>
+      <section className="tasks-section">
+         <Card credImg={daoImg} data={getTaskData("DAO", data)} title="DAOs" />
+         <Card credImg={defiImg} data={getTaskData("DEFI", data)} title="DEFI" />
+         <Card credImg={nftImg} data={getTaskData("NFT", data)} title="NFTs" />
+         <Card credImg={l2Img} data={getTaskData("L2", data)} title="L2"/>
+      </section>
    );
 }
 
 export default Tasks;
-
-// return (
-//    <div className="outer-table">
-//       <div className="table-container">
-//       <table
-//          className={`table table-striped table-bordered table-sm ${
-//             darktheme && "table-dark"
-//          }`}
-//       >
-//          <thead>
-//             <tr>
-//                <th className="left" scope="col">
-//                   Task
-//                </th>
-//                <th scope="col" width="6.875rem" className="px-3">
-//                   Progress
-//                </th>
-//                <th scope="col">Redeemed</th>
-//             </tr>
-//          </thead>
-//          <tbody>
-//             {data.length > 0 ? (
-//                data.map((item, i) => (
-//                   <tr
-//                      className={item[1].isDisabled ? "table-danger" : ""}
-//                      key={`task-${i}`}
-//                   >
-//                      <th scope="row">{formatName(item[0])}</th>
-//                      <td align="center">{item[1].progress ? "✅" : "❌"}</td>
-//                      <td align="center">{(item[1].progress && !legacyTasks.includes(item[0])) || item[1].redeemed ? "✅" : "❌"}</td>
-//                   </tr>
-//                ))
-//             ) : (
-//                <tr>
-//                   <td colSpan="3" className="text-center">
-//                      <p className="py-5">No tasks found.</p>
-//                   </td>
-//                </tr>
-//             )}
-//          </tbody>
-//       </table>
-//       </div>
-//    </div>
-// );
